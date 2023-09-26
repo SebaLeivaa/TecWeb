cd980f037e5a8a9d24c1fba41e9f5070d0c54179
SebastianLeivaa <sebastianleiva26@gmail.com>
2023-09-26 20:34:42 -0300

    Organizo mi repositorio y subo los primeros dos ejercicios de la guia

--- javascript/password.js
+++
@@ -1,4 +0,0 @@
-/*Crear un programa que pida al usuario una contraseña. Luego, verificar si la contraseña
-ingresada cumple con ciertos criterios, como tener al menos 8 caracteres, incluir al menos
-una letra mayúscula y un número. Muestra un mensaje en la consola indicando si la
-contraseña es válida o no. */
