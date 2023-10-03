5b63610f75b4c0abdc31f2048498b282e350aa48
SebaLeivaa <sebastiannicolas.leiva@alumnos.ulagos.cl>
2023-10-03 15:45:55 -0300

    Actualizando reto 3

--- retos/reto3/javascript/reto.js
+++ retos/reto3/javascript/reto.js
@@ -2,6 +2,7 @@ function calcularPropina(){
     const inputMonto = document.getElementById("monto");
     const inputPorcentaje = document.getElementById("porcentaje-propina");
     const errorMonto = document.getElementById("error-monto");
+    const errorMonto2 = document.getElementById("error-monto2");
     const exitoCalcular = document.getElementById("exito-calcular");
     const propinaSpan = document.getElementById("monto-propina");
     const totalPagoSpan = document.getElementById("total-pago");
@@ -11,11 +12,13 @@ function calcularPropina(){
 
     if(monto < 500 || monto > 500000 || isNaN(monto)){
         errorMonto.textContent = "Por favor, ingrese un monto valido!";
+        errorMonto2.textContent = "Su monto debe estar entre $500 a $500000.";
         exitoCalcular.textContent = "";
     }else{
         errorMonto.textContent = "";
+        errorMonto2.textContent = "";
         exitoCalcular.textContent = "Su propina ha sido calculada exitosamente!";
-        const montoPropina = monto*porcentaje;
+        const montoPropina = Math.round( monto*porcentaje);
         const totalPagar = montoPropina + monto;
         propinaSpan.textContent = `$${montoPropina} CLP`;
         totalPagoSpan.textContent = `$${totalPagar} CLP`;
