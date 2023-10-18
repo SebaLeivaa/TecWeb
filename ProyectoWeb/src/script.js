6341024def84defecc2d323cf8760550a8a4d00c
SebastianLeivaa <sebastianleiva26@gmail.com>
2023-10-17 21:30:45 -0300

    Subiendo segundo avance de proyecto

---
+++ ProyectoWeb/src/script.js
@@ -0,0 +1,93 @@
+function loginButton() {
+  const loginDialog = document.getElementById("loginDialog");
+  const signinDialog = document.getElementById("signinDialog");
+  signinDialog.close();
+  loginDialog.showModal();
+
+  const lightDismiss = ({ target: loginDialog }) => {
+    if (loginDialog.nodeName === "DIALOG") loginDialog.close("dismiss");
+  };
+
+  loginDialog.addEventListener("click", lightDismiss);
+}
+
+function signinButton() {
+  const loginDialog = document.getElementById("loginDialog");
+  const signinDialog = document.getElementById("signinDialog");
+  loginDialog.close();
+  signinDialog.showModal();
+
+  const lightDismiss = ({ target: signinDialog }) => {
+    if (signinDialog.nodeName === "DIALOG") signinDialog.close("dismiss");
+  };
+
+  signinDialog.addEventListener("click", lightDismiss);
+}
+
+function closeDia() {
+  const loginDialog = document.getElementById("loginDialog");
+  const signinDialog = document.getElementById("signinDialog");
+  loginDialog.close();
+  signinDialog.close();
+}
+
+function login() {
+  alert("test login");
+
+  const ident = document.getElementById();
+  const pass = document.getElementById();
+  //Si ident y pass son correctos, se inicia sesion
+}
+
+function reConfirm(form){
+    const name = form.paName.value;
+    const lastname = form.paLName.value;
+    const rut = form.paRut.value;
+    const bd = form.DoB.value;
+    const email = form.paEmail.value;
+    const pass = form.paPassword.value;
+    const gender = form.paGender.value;
+    const phone = form.paPhone.value;
+    console.log('name: '+name+"\nlastname: "+lastname+'\nrut: '+rut+'\nbd: '+bd+'\nemail: '+email+'\npass: '+pass+'\ngender: '+gender+'\nphone: '+phone)
+}
+
+function loConfirm(form){
+    const ident = form.rutEmail.value
+    const pass = form.loPass.value
+    console.log('ident: '+ ident+ 'pass: '+pass)
+}
+
+function signin() {
+  alert("test signin");
+
+  
+  //Recibe los datos y los guarda
+}
+
+/* descarga un archivo de texto
+
+function CreateTextFile() {
+    var blob = new Blob(["This is a sample file content."], {
+       type: "text/plain;charset=utf-8",
+    });
+    saveAs(blob, "download.txt");
+ }
+*/
+
+/*
+function inicio(){
+
+}
+
+function especialidades(){
+
+}
+
+function profesionales(){
+
+}
+
+function contacto(){
+
+}
+*/
--- ProyectoWeb/tailwind.config.js
+++ ProyectoWeb/tailwind.config.js
@@ -3,13 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
-      primary: '#007BFF',
-      secondary: '#69a9ff',
-      header: '#00619a',
-      text: '#FFFFFF',
-      border: '#000000',
+    primary: '#007BFF',
+    secondary: '#69a9ff',
+    header: '#00619a',
+    text: '#FFFFFF',
+    border: '#000000',
+    grayborder: '#A9A9A9',
+  },
+    extend: {
+      
     },
-    extend: {},
   },
   plugins: [],
 }
